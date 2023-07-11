6634fec779fcd6c5bbacdda38157724dd56981ee
ShabdKumar <shabdkumarsahu@gmail.com>
2023-07-11 12:20:51 +0530

    Displaying Recipe Details, Added IngredientModel, ShoppingList and Edit

---
+++ Course-Projects/RecipeBook/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css'],
+})
+export class RecipeListComponent implements OnInit {
+  recipesList: Recipe[] = [
+    new Recipe(
+      'Dummy Recipe',
+      'Just for Test Purpose',
+      'https://st2.depositphotos.com/3889193/7173/i/450/depositphotos_71739083-stock-photo-healthy-vegetarian-home-made-food.jpg'
+    ),
+    new Recipe(
+      'Dummy Recipe1',
+      'Just for Test Purpose1',
+      'https://st2.depositphotos.com/3889193/7173/i/450/depositphotos_71739083-stock-photo-healthy-vegetarian-home-made-food.jpg'
+    ),
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
