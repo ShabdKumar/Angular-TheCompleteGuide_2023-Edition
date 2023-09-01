5a07ebaa2e96ec1005cb63b89497755110fdf3ba
ShabdKumar <shabdkumarsahu@gmail.com>
2023-09-01 16:13:41 +0530

    Finished

--- Learnings/routing/src/app/error-page/error-page.component.ts
+++ Learnings/routing/src/app/error-page/error-page.component.ts
@@ -5,21 +5,19 @@ import { Subscription } from 'rxjs/internal/Subscription';
 @Component({
   selector: 'app-error-page',
   templateUrl: './error-page.component.html',
-  styleUrls: ['./error-page.component.css']
+  styleUrls: ['./error-page.component.css'],
 })
-export class ErrorPageComponent implements OnInit, OnDestroy{
+export class ErrorPageComponent implements OnInit, OnDestroy {
   message: string;
-  subscription:Subscription;
+  subscription: Subscription;
 
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
     this.message = this.route.snapshot.data['message'];
-    this.subscription = this.route.data.subscribe(
-      (data: Data) => {
-        this.message = data['message'];
-      }
-    );
+    this.subscription = this.route.data.subscribe((data: Data) => {
+      this.message = data['message'];
+    });
   }
 
   ngOnDestroy(): void {
